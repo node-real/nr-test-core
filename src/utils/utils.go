f63aab4acb11c13d34bf0abce253c85e8a4d0fe6
robertw07 <112619961+robertw07@users.noreply.github.com>
2022-12-14 18:43:13 +0800

    update tryfunc api

--- src/utils/utils.go
+++ src/utils/utils.go
@@ -1,6 +1,10 @@
 package utils
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"github.com/node-real/nr-test-core/src/log"
+)
 
 type Utils struct {
 	MathUtils
@@ -13,15 +17,17 @@ func RunFunWithRetry(f func() error, retryCount int) error {
 	for i := 0; i <= retryCount; i++ {
 		func() {
 			defer func() {
-				if err := recover(); err != nil {
-					fmt.Println("panic error:", err)
+				err1 := recover()
+				if err1 != nil {
+					err = errors.New(fmt.Sprint("panic error:", err1))
+					log.Error(fmt.Sprint("panic error:", err1))
 				}
 			}()
 			err = f()
-			if err == nil {
-				return
-			}
 		}()
+		if err == nil {
+			break
+		}
 	}
 	return err
 }
